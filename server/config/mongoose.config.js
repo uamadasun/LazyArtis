1b551dfa9dfc2d7d8867a223478cade31315a0fc
uamadasun <uchennaamadasun@gmail.com>
2023-02-09 14:20:12 -0800

    deleted s3.js and some unnecessary files/code

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -5,26 +5,3 @@ mongoose.connect("mongodb://localhost/users", {
 })
     .then(() => console.log("Established a connection to the database"))
     .catch(err => console.log("Something went wrong when connecting to the database", err));
-
-
-
-
-
-
-
-
-
-
-
-// const mongoose = require("mongoose");
-
-// module.exports = db_name => {
-//     mongoose
-//         .connect(`mongodb://localhost/${db_name}`, {
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true,
-//             useFindAndModify: false
-//         })
-//         .then(() => console.log(`Successfully connected to "${db_name}" database`))
-//         .catch(err => console.log("mongoose connection failed: ", err));
-// };
--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -8,18 +8,7 @@ module.exports = function(app){
     app.get("/api/user/loggedin/:_id", authenticate, UserController.getLoggedInUser);
     app.put('/api/image/:_id', UserController.addImageToArray);
     app.patch('/api/delete/image/:_id/:url', authenticate, UserController.deleteImageFromAllImages)
-
-    
     app.get("/api/users", UserController.getUsers);
     app.get("/api/user/:_id", authenticate, UserController.findOneUser);
     app.delete('/api/user/:_id', UserController.deleteUser);
-
-    // app.post("/api/login", UserController.login);
-    // app.post('/api/create/user', UserController.register);
-    // app.get('/api/user', authenticate, UserController.index);  //add authenticate back in after route
-    // app.get('/api/user/:id', authenticate, UserController.show); //add authenticate back in after route
-    // app.put('/api/update/user/:id', authenticate, UserController.update); //add authenticate back in after route
-    // app.delete('/api/destroy/user/:id', authenticate, UserController.destroy); //add authenticate back in after route
-    // app.get("/api/logout", authenticate, UserController.logout);                    //need to add 'user' to path?  '/api/user/logout?
-    // app.get("/api/user/loggedin", authenticate, UserController.getLoggedInUser);
 }
